2af4f07e86cfd75b06ed41c1aaefc96f77c296ef
ih8unem <ih8unem@gmail.com>
2018-02-04 19:06:49 +0300

    js version

--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,25 +1,31 @@
-@file:Suppress("DEPRECATION")
-
 import kotlin.js.Math.random
 
-class Player(private val nw: Network, private val predicateMoves: Int = 4,
-             val error: Double=3.0) {
+class Player(val nw: Network, private val predicateMoves: Int = 2,
+             val error: Double = 0.0) {
     /**
      * Выбор ИИ наилучшего хода
      * [checkerboard] - доска на момент хода
      * [color] - цвет фигур ИИ
      * [steps] - набор возможных ходов
      *
-     * Оценка каждого хода производится в паралельных потоках
+     * Оценка каждого хода производится в параллельных потоках
      * Оценка осуществляется путём проигрывания ходов вперёд в количестве [predicateMoves]
      * В итоге получается список пар {ход - счёт}
      * Выбирается ход с максимальным счётом
      */
     fun selectMove(checkerboard: Checkerboard, color: Int, steps: List<String>): String {
-        val list = steps.map { it to play(checkerboard, color, predicateMoves, it) }.toList() // оценка каждого ходв
-        val max = list.maxBy { it.second }!!.second
+        if (steps.size == 1) return steps.first()
+        val pairs = stepsToPairs(steps, checkerboard)
+        if (predicateMoves == 0) {
+            return if (color == 0) pairs.sortedBy { it.second }.last().first
+            else pairs.sortedBy { it.second }.first().first
+        }
+        val goodPairs = filterGoodSteps(pairs, color)
+        val list = goodPairs.map {
+            it to play(checkerboard, color, predicateMoves, it.first) }.toList()
+        val max = list.maxBy { it.second }?.second
         val l = list.filter { it.second == max }.map { it.first }
-        val step = selectBestStep(checkerboard, color, l)
+        val step = selectBestStep(l, color)
         return step
     }
 
@@ -33,8 +39,8 @@ class Player(private val nw: Network, private val predicateMoves: Int = 4,
      * Начинает с заданного хода
      * По окончанию ходов или по результатам партии (если до выделенного количество ходов игра окончена)
      * происходит подсчёт очков
-     * За выйгранную партию начисляется 100 очков, за проигрыш - минус 100
-     * Чтобы выбрать, который приблежает победу или оттягивает поражение, из 100 вычтим количество ходов, затраченных в игре
+     * За выигранную партию начисляется 100 очков, за проигрыш - минус 100
+     * Чтобы выбрать, который приближает победу или оттягивает поражение, из 100 вычтем количество ходов, затраченных в игре
      * Если результат не определен, очки начисляются за оставшееся количество шашек
      * стоимость шашки - 1 очко, дамки - 3
      * Результат определяется как разность очков между белыми и чёрными
@@ -42,16 +48,18 @@ class Player(private val nw: Network, private val predicateMoves: Int = 4,
     private fun play(checkerboard: Checkerboard, initColor: Int, count: Int, initStep: String): Int {
         val game = GameController(checkerboard.clone())
         game.currentColor = initColor
-        var steps = game.nextMoves()
+        lateinit var steps: List<Pair<String, Double>>
         for (i in 0 until count * 2) {
-            val step = if (i == 0) initStep else selectBestStep(game.checkerboard, game.currentColor, steps)
+            val step = if (i == 0) initStep else {
+                steps = filterGoodSteps(stepsToPairs(game.nextMoves(), game.checkerboard), game.currentColor)
+                if (steps.isEmpty()) {
+                    return if (game.currentColor != initColor) 100 - i
+                    else -100 + i
+                }
+                selectBestStep(steps, game.currentColor)
+            }
             game.go(step)
             game.currentColor = 1 - game.currentColor
-            steps = game.nextMoves()
-            if (steps.isEmpty()) {
-                return if (game.currentColor != initColor) 100 - i
-                else -100 + i
-            }
         }
         val whiteCount = game.checkerboard.encodeToVector().filter { it > 0 }.count()
         val blackCount = game.checkerboard.encodeToVector().filter { it < 0 }.count()
@@ -61,39 +69,46 @@ class Player(private val nw: Network, private val predicateMoves: Int = 4,
 
     /**
      * Выбор ИИ наилучшего хода в процессе оценки,
-     * если выберать неизчего возвращаем результат
+     * если выбирать неизчего возвращаем результат
      *
      * Алгоритм
      * Для каждого хода из заданного списка
      * Положение шашек на доске кодируется в вектор действительных чисел.
-     * Размер вектора соответсвует игровым полям доски, т.е. 32
-     * Этот вектор дальше преобразуется при помощи инкодера в вектор с размерностью 91.
-     * Инкодер составляет вектор из фрагментов доски по аналогии с принципом работы свёрточных сетей.
-     * Это позволяет нейронной сети, как бы рассматривать доску с разных точек зрения, а нейроны её первого слоя,
-     * тогда играют роль детектора фич
-     * При помощи мультиактивации, каждый вектор массива сопоставляется с одним нейроном сети первого слоя
-     * Каждый нейрон выступает своего рода рецептором, который реагирует на ситуацию на доске в общем или какойто ее части
-     * Дальше нейронная сеть вычисляет число от -1 до 1, где 1 соответствуют лучшему с её точки зрения ходу, а -1 - худшему
-     *
-     * Чтобы нейронная сеть не зависила от цвета фигур за которые она играет, оценка идёт со стороны белых
+     * Нейронную сеть активируем этим вектором
+     * Дальше нейронная сеть вычисляет число от -1 до 1, где 1 соответствуют лучшему с её точки зрения ходу, а -1 - худшему     *
+     * Чтобы нейронная сеть не зависела от цвета фигур за которые она играет, оценка идёт со стороны белых
      * Если нейронная сеть играет за чёрных, то выбирается ход наиболее худший для белых (т.е. минимальное значение)
      * Если за белых, то соответственно максимальное значение
+     * Для ускорения выбора хода, отсеем по медиане худшие значения, а на оставшиеся проиграем игру на два хода вперёд
+     * Сеть конечно определяет ход противника, исходя из своих значений весов
      **/
-    private fun selectBestStep(checkerboard: Checkerboard, color: Int, steps: List<String>): String {
+    private fun selectBestStep(steps: List<Pair<String, Double>>, color: Int): String {
         if (steps.isEmpty()) return ""
-        if (steps.size == 1) return steps[0]
-        val list = steps.map { it to GameController(checkerboard.clone()) }.map { (command, game) ->
-            // может стоить запаралелить
-            game.go(command)
-            val vector = game.checkerboard.encodeToVector()
-            val o = nw.multiActivate(InputEncoder().encode(vector))
-            command to o[0]
-        }.toList().map {
+        if (steps.size == 1) return steps.first().first
+        val list = steps.map {
             it.first to it.second * if (error > 0) (1 + error/100 * (1 - 2*random())) else 1.0
-        }
+        } // закладываем ошибку исключить повтор ходов
         val step = (if (color == 0) {
             list.maxBy { it -> it.second }!!
         } else list.minBy { it.second }!!)
         return step.first
     }
+
+    private fun stepsToPairs(steps: List<String>, checkerboard: Checkerboard) = steps.map {
+        val game = GameController(checkerboard.clone())
+        game.go(it)
+        it to nw.cnn(game.checkerboard.encodeToVector())
+    }.toList()
+
+    private fun filterGoodSteps(steps: List<Pair<String, Double>>, color: Int): List<Pair<String, Double>> {
+        if (steps.size < 3) return steps
+        val median = steps.map { it.second }.median()
+        return if (color == 0) {
+            steps.filter { it.second >= median }
+        } else {
+            steps.filter { it.second <= median }
+        }
+    }
 }
+
+private fun List<Double>.median() = sorted()[size/2]
--- src/main/kotlin/network.kt
+++ src/main/kotlin/network.kt
@@ -1,3 +1,6 @@
+import kotlin.js.Math.random
+import kotlin.math.max
+import kotlin.math.sqrt
 import kotlin.math.tanh
 
 class Neuron {
@@ -7,37 +10,79 @@ class Neuron {
 
     // активационная функция
     fun activate(input: List<Double>): Double {
+        if (input.size + 1 != weights.size) { // инициализация весов в случае изменения топологии сети
+            setRandomWeights(input.size + 1)
+        }
         val x = listOf(1.0, *input.toTypedArray()) // добавляем вход 1 для смещения
         return a*tanh(b*sum(x))
     }
 
+    fun setRandomWeights(size: Int) {
+        weights = initWeights(size)
+    }
+
     // сумматор
-    private fun sum(input: List<Double>) = weights.mapIndexed { i, w -> input[i] * w }.sum()
+    fun sum(input: List<Double>) = weights.mapIndexed { i, w -> input[i] * w }.sum()
+
+    // инициализация весов случайными значениями
+    private fun initWeights(inputSize: Int) = MutableList(inputSize, { 0.5 - random() })
 }
 
 class Layer(size: Int=0) {
     val neurons = MutableList(size, { Neuron() })
     fun activate(input: List<Double>) = neurons.map { it.activate(input) }
+
+    fun cnn(input: List<List<Double>>, filterSize: Int, shift: Int): List<List<Double>> {
+        val x = input.map {
+            Encoder.divideToMatrix(it, filterSize, shift)
+        }
+        val y = (0 until x.first().size).map { i->
+            (0 until x.size).flatMap { j ->
+                x[j][i]
+            }
+        }
+        return neurons.map { kernel -> y.map {
+            if (kernel.weights.size != it.size) kernel.setRandomWeights(it.size)
+            kernel.sum(it)
+        } }
+    }
 }
 
 class Network(vararg layerSize: Int) {
     val layers = MutableList(layerSize.size, { i -> Layer(layerSize[i]) })
 
-    fun multiActivate(x: List<List<Double>>): List<Double> {
-        synchronized(this) {
-            layers[0].neurons.apply {
-                if (size != x.size) {
-                    clear()
-                    addAll(List(x.size, { Neuron() }))
-                }
-            }
-        }
-        var y = layers[0].neurons.mapIndexed { i, neuron ->
-            neuron.activate(x[i])
-        }
-        for (i in 1 until layers.size) {
+    fun activate(input: List<Double>): List<Double> {
+        var y = input
+        // последовательно активируем все слои
+        for (i in 0 until layers.size) {
             y = layers[i].activate(y)
         }
         return y
+    }
+
+    fun cnn(x: List<Double>): Double {
+        var y = layers[0].cnn(listOf(x), 3, 1).map { relu(it) }
+        y = layers[1].cnn(y, 5, 1).map { relu(it) }.map { pool(it) }
+        var o = y.flatMap { it }
+        for (i in 2 until layers.size) {
+            o = layers[i].activate(o)
+        }
+        return o.first()
+    }
+
+    fun relu(x: List<Double>) = x.map { max(it, 0.0) }
+
+    fun pool(x: List<Double>) =  Encoder.divideToMatrix(x, 2, 2).map { it.max()!! }
+}
+
+object Encoder {
+    private fun <T> conv(input: List<T>, x0: Int, side: Int, matrixSize: Int) = List(side, { i: Int -> List(side, { input[x0 + it + matrixSize * i] }) }).flatMap { it }
+
+    fun <T>divideToMatrix(x: List<T>, size: Int, shift: Int): List<List<T>> {
+        val inputSide = sqrt(x.size.toDouble()).toInt()
+        return (0..inputSide-size step shift).map {row ->
+            (0..inputSide - size step shift)
+                    .map { conv(x, it +row*inputSide, size, inputSide) }
+        }.flatMap { it }
     }
 }
